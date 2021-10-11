672081319f2ba37181fec3364dd6bf9e775e2d2e
Alexkim94 <tkim51@gmu.edu>
2021-10-12 08:20:30 +0900

    update.10.12.21

--- Assets/Scripts/ExamplePlayerMovement.cs
+++ Assets/Scripts/ExamplePlayerMovement.cs
@@ -10,15 +10,19 @@ public class ExamplePlayerMovement : MonoBehaviour {
     {
         // Rotate the player by pressing left or right
         if (FigmentInput.GetButton(FigmentInput.FigmentButton.LeftButton))
+            //how to make player move only to the left>
         {
             transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         }
+        //how to make player move only to the right>
+
         else if (FigmentInput.GetButton(FigmentInput.FigmentButton.RightButton))
         {
             transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
         }
 
         // If we press the action button, move forward
+        //how to transform Player to playable brick??
         if (FigmentInput.GetButton(FigmentInput.FigmentButton.ActionButton))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
